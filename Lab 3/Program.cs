8f6fb5dd5218b436c96967cc6cb892f752f07029
TxnUkeGrl <txnukegrl@gmail.com>
2021-04-10 21:10:48 -0400

    second commit, final product 4/10/21

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -5,27 +5,6 @@ namespace Lab_3
     class Program
     {
         static void Main(string[] args)
-        /*Task: Use conditional statements to automate the decision-making process.
-            What will the application do?
-                ● 1 Point: The application prompts to the user to enter an integer between 1 and 100.
-                ● The application displays the associated result based on the integer range entered.
-            Build Specifications:
-                ● 1 Point: Use if/else statements to make different actions depending on user input.
-                ● Given an integer entered by a user, perform the following conditional actions:
-                    ○ 1 Point: If the integer is odd, print the number entered and “Odd.”
-                    ○ 1 Point: If the integer entered is even and in the inclusive range of 2 to 25, print “Even and less than 25.”
-                    ○ 1 Point: If the integer entered is even and in the inclusive range of 26 to 60, print “Even.”
-                    ○ 1 Point: If the integer entered is even and greater than 60, print the number entered and “Even”.
-                    ○ 1 Point: If the integer entered is odd and greater than 60, print the number entered and “Odd.”
-            Additional Requirements:
-                ● 1 Point: For answering Lab Summary when submitting to the LMS
-                ● -2 Points: if there are any syntax errors or if the program does not run (for example, in a Main method).
-            Extended Exercises (2 points maximum):
-                ● 1 Point: Include a set of parameters so that the program ends officially.
-                ● 1 Point: Ask for user information (ex. name) at the beginning of the application, and use it to refer to the user 
-                           throughout the application.
-                ● 1 Point: Add validation to guarantee that a user enters a positive integer between 1 and 100.
-        */
         {
             Console.WriteLine("Hello User! What is your name?");
             string EntryName = Console.ReadLine();
@@ -73,7 +52,7 @@ namespace Lab_3
                     Console.WriteLine("Yeeaaa.......noooo......that's not gonna work " + EntryName + ". GG.");
                     //Console.ReadLine();
                 }
-                Console.WriteLine("Would you like to go again " + EntryName + "? (y/n)");
+                Console.WriteLine("Would you like to go again " + EntryName + "? Press \"Y\" to continue!");
             }
             while (Console.ReadLine().ToLower() == "y");
 
